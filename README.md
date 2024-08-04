4a5ff74a948a0401073a69924814a97d97c4da7a
frozar <fabien.rozar@gmail.com>
2024-08-04 16:28:12 +0400

    Scaffold UI

--- README.md
+++ README.md
@@ -2,16 +2,15 @@
 
 ## TODO
 
- - [ ] Make it deploy (vercel)
- - [ ] Scaffold basic ui with mock data
- - [ ] Actually set up a database (vercel postgres)
- - [ ] Attach database to UI
- - [ ] Add authentification (w/ clerk)
- - [ ] Add image upload
- - [ ] Error management (w/ Sentry)
- - [ ] Routing/image page (parallel route)
- - [ ] Delete button (w/ Server Actions)
- - [ ] Analytics (posthog)
- - [ ] Ratelimiting (upstash)
- - [ ] 
- 
+- [x] Make it deploy (vercel)
+- [x] Scaffold basic ui with mock data
+- [ ] Actually set up a database (vercel postgres)
+- [ ] Attach database to UI
+- [ ] Add authentification (w/ clerk)
+- [ ] Add image upload
+- [ ] Error management (w/ Sentry)
+- [ ] Routing/image page (parallel route)
+- [ ] Delete button (w/ Server Actions)
+- [ ] Analytics (posthog)
+- [ ] Ratelimiting (upstash)
+- [ ]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/d39e0bba-eb4e-4307-9df1-e6f1f304b0da-uxui2z.webp",
+  "https://utfs.io/f/c063c438-a226-48d2-a21e-a52dd8e6de67-2ebjy4.webp",
+  "https://utfs.io/f/c38f1f6f-c82a-4647-9846-fcea670b615d-hnnlbr.webp",
+  "https://utfs.io/f/02d97d28-008f-447c-a3fe-9e6173313df8-x4q2y6.webp",
+];
+
+const mockImages = mockUrls.map((url, index) => ({ id: index + 1, url }));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt={image.id.toString()} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
